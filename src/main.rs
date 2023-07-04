21c43897d13c0252150257a4965540a3052152dd
Léo FASANO <fasanol.info@gmail.com>
2023-07-04 12:07:16 +0200

    doc: add some code documentation

--- src/main.rs
+++ src/main.rs
@@ -76,7 +76,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     };
 
     /* -------------------------------------------------------------------------- */
-    /*                              Send transactions                             */
+    /*                          Setup transactions calls                          */
     /* -------------------------------------------------------------------------- */
 
     // Craft the transaction
@@ -85,8 +85,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let to = Address::from_slice(&hex::decode(to.as_str().unwrap()).expect("Decoding failed"));
 
     // Create a vector to hold all our pending futures.
-    let mut pending_txs = Vec::new();
+    let mut pending_block_txs = Vec::new();
 
+    // Set the signer nonce to 0 to allow nonce "prediction"
     web3.transport()
         .execute(
             "anvil_setNonce",
@@ -95,14 +96,25 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .await?;
     println!("\x1b[32m✔\x1b[0m Nonce set to 0.");
 
+    // Declare transaction counters
     let mut succeeded_txs = 0;
     let mut failed_txs = 0;
 
-    for nonce in 0..tx_amount.as_u64().unwrap() {
-        if nonce % tx_per_block.as_u64().unwrap() == 0 && nonce != 0 {
-            let result =
-                mine_and_wait(&web3, pending_txs, block_mining_ms_pause.as_u64().unwrap()).await;
+    /* -------------------------------------------------------------------------- */
+    /*                              Send transactions                             */
+    /* -------------------------------------------------------------------------- */
 
+    for nonce in 0..tx_amount.as_u64().unwrap() {
+        // If we have reached the tx_per_block limit, mine a block and wait block_mining_ms_pause
+        if nonce % tx_per_block.as_u64().unwrap() == 0 && nonce != 0 {
+            let result = mine_and_wait(
+                &web3,
+                pending_block_txs,
+                block_mining_ms_pause.as_u64().unwrap(),
+            )
+            .await;
+
+            // Update transaction counters
             match result {
                 Ok((succeeded, failed)) => {
                     succeeded_txs += succeeded;
@@ -111,9 +123,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 Err(_) => println!("Error while mining block"),
             }
 
-            pending_txs = Vec::new();
+            // Reset the pending_block_txs vector
+            pending_block_txs = Vec::new();
         }
 
+        // Craft the transaction
         let tx_request = TransactionRequest {
             from: my_account,
             to: Some(to),
@@ -123,12 +137,20 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             ..Default::default()
         };
 
+        // Send the transaction and add it to the pending_block_txs vector
         let pending_tx = web3.eth().send_transaction(tx_request);
-        pending_txs.push(pending_tx);
+        pending_block_txs.push(pending_tx);
     }
 
-    let result = mine_and_wait(&web3, pending_txs, block_mining_ms_pause.as_u64().unwrap()).await;
+    // Mine the last block
+    let result = mine_and_wait(
+        &web3,
+        pending_block_txs,
+        block_mining_ms_pause.as_u64().unwrap(),
+    )
+    .await;
 
+    // Update transaction counters
     match result {
         Ok((succeeded, failed)) => {
             succeeded_txs += succeeded;
@@ -137,6 +159,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         Err(_) => println!("Error while mining block"),
     }
 
+    /* -------------------------------------------------------------------------- */
+    /*                                   Result                                   */
+    /* -------------------------------------------------------------------------- */
+
+    // Print the succeded transaction result
     if succeeded_txs > 0 {
         println!(
             "\n\x1b[32m✔\x1b[0m \x1b[35m{} transactions\x1b[0m has been sent in \x1b[36m{} blocks\x1b[0m with \x1b[33m{} ms\x1b[0m pause between each blocks.",
@@ -146,13 +173,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         );
     }
 
+    // Print the failed transaction result
     if failed_txs > 0 {
-        println!("\x1b[31m✖\x1b[0m {} failed transactions (Mempool probably full, try empty it)", failed_txs);
+        println!(
+            "\x1b[31m✖\x1b[0m {} failed transactions (Mempool probably full, try empty it)",
+            failed_txs
+        );
     }
 
     Ok(())
 }
 
+/// Mine a block, wait for the block to be mined and sleep for the specified amount 
+/// of time (config.block_mining_ms_pause).
 async fn mine_and_wait(
     web3: &Web3<WebSocket>,
     pending_txs: Vec<
@@ -168,11 +201,14 @@ async fn mine_and_wait(
     >,
     pause_ms: u64,
 ) -> Result<(u64, u64), Box<dyn std::error::Error>> {
+    // Declare transaction counters
     let mut succeeded_txs = 0;
     let mut failed_txs = 0;
 
+    // Wait for the transaction to be indexed
     let results = join_all(pending_txs).await;
 
+    // Update transaction counters
     for result in results {
         match result {
             Ok(_) => succeeded_txs += 1,
@@ -180,19 +216,24 @@ async fn mine_and_wait(
         }
     }
 
+    // Mine a block
     let result = web3
         .transport()
         .execute("anvil_mine", vec![serde_json::to_value(1)?])
         .await;
 
+    // Print the result
     match result {
-        Ok(_) => println!("👉 \x1b[32mBlock mined\x1b[0m with {} transactions", succeeded_txs),
+        Ok(_) => println!(
+            "👉 \x1b[32mBlock mined\x1b[0m with {} transactions",
+            succeeded_txs
+        ),
         Err(_) => println!("👉 \x1b[32mBlock mining failed\\x1b[31m"),
     }
 
-    // web3.transport().execute("anvil_dropTransaction", vec![]).await?;
-    // Sleep for the specified amount of time
-    std::thread::sleep(std::time::Duration::from_millis(pause_ms + 500));
+    // sleep for the specified amount of time
+    std::thread::sleep(std::time::Duration::from_millis(pause_ms));
 
+    // Return the transaction counters
     Ok((succeeded_txs, failed_txs))
 }
